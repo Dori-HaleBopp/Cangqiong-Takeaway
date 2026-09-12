b8a5fbf930ea0cca7cddbe33c404002e91a742be
halebopp <1794636978@qq.com>
2026-09-12 18:27:33 +0800

    新增套餐管理功能

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -113,7 +113,13 @@ public class DishServiceImpl implements DishService {
             });
             dishFlavorMapper.insertBatch(flavors);
         }
+    }
 
-
+    public List<Dish> list (Long categoryId){
+        Dish dish = Dish.builder()
+                .categoryId(categoryId)
+                .status(StatusConstant.ENABLE)
+                .build();
+        return dishMapper.list(dish);
     }
 }
